1a730a678740d07239d67ad99c4438cea59a644d
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2024-12-11 08:17:08 +0100

    Learning flutter by building projects from scratch

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,29 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+// ignore: must_be_immutable
+class GradientContainer extends StatelessWidget {
+  GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
