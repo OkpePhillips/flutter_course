6508225bb676110c6603409e11e347e889b762e5
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2024-12-21 21:07:15 +0100

    Building an expense tracker and using more features of flutter'

---
+++ expense_tracker/lib/widgets/expenses.dart
@@ -0,0 +1,44 @@
+import 'package:expense_tracker/widgets/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter Course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: ExpenseCategory.work,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 15.60,
+      date: DateTime.now(),
+      category: ExpenseCategory.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+      appBar: AppBar(),
+    );
+  }
+}
